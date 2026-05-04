cca473f0844a480811a6d74a5bea1d6b7ae7d6ee
Javierzrierag <javierzrierag@gmail.com>
2026-05-04 03:40:31 -0300

    test secrets

--- cypress/e2e/ui/firstTest.cy.ts
+++ cypress/e2e/ui/firstTest.cy.ts
@@ -7,14 +7,21 @@ beforeEach(() => {
 describe('Login Tests', () => {
 
   it('Login con UI', () => {
-    cy.loginUI(Cypress.env('USER'), Cypress.env('PASSWORD'))
+    cy.loginUI(
+      Cypress.env('USER'),
+      Cypress.env('PASSWORD')
+    )
     cy.contains('The Dummy Site').should('be.visible')
   })
 
   it('Login con API (sin UI)', () => {
-    cy.loginAPI(Cypress.env('USER'), Cypress.env('PASSWORD'))
+    cy.loginAPI(
+      Cypress.env('USER'),
+      Cypress.env('PASSWORD')
+    )
     cy.visit('/dashboard')
     cy.contains('The Dummy Site').should('be.visible')
   })
 
 })
+
